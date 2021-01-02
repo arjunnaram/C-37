652f9d3915ba35e00601210b58b93b9cad61d724
arjunnaram <surajnaram1@gmail.com>
2021-01-02 10:30:39 -0500

    i am bst

--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ var canvas, backgroundImage;
 
 var gameState = 0;
 var playerCount;
-
+var distance=0
 var database;
 
 var form, player, game;
+
+var allPlayers;
 
 
 function setup(){
@@ -18,4 +20,17 @@ function setup(){
 
 
 function draw(){
+
+  if(playerCount===4){
+    game.update(1)
+
+  }
+  if(gameState===1){
+
+    clear()
+    game.play()    
+
+  }
+
+
 }
